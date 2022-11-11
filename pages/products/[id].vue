3156dcd2411701932defed757d74ef50808e2467
jono_oliver <jonathan.oliver@visualsoft.co.uk>
2022-11-11 15:10:47 +0000

    Adding sandbox to git

---
+++ pages/products/[id].vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ProductDetails :product="product" />
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+
+const { id } = useRoute().params,
+  url = "https://fakestoreapi.com/products/" + id;
+
+const { data: product } = await useFetch(url, { key: id });
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <div v-for="product in products">
+      <ProductCard :product="product" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+
+<style scoped></style>
