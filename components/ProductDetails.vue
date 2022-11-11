1cf9caf66435c5a46000b4722539fd9a20405da8
jono_oliver <jonathan.oliver@visualsoft.co.uk>
2022-11-11 15:38:50 +0000

    grid change

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <div class="grid grid-cols-2 gap-10">
+    <div class="grid lg:grid-cols-2 gap-10">
       <div class="p-7">
         <img :src="`${product.image}`" class="mx-auto my-7" />
       </div>
